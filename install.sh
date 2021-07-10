6d34d0789995a89675d1b1770e3a6c6483b55224
Akusem <nathanael.z@outlook.fr>
2021-07-10 12:23:02 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,23 @@
+# install zsh
+sudo apt install zsh
+
+# install oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Download Fish like extension
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install spaceship theme
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+# Activate plugins
+sed -i -e 's/plugins = (git)/plugins = ( git zsh-autosuggestions history-substring-search zsh-syntax-highlighting) ~/.zshrc'
+
+# Activate theme
+sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="spaceship" ~/.zshrc'
+
+# Reload terminal
+source ~/.zshrc
